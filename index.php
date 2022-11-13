2ebe7d9acaad9420fe4e06cc553acf8dea1eacbd
ArletaGavrilova <109897868+ArletaGavrilova@users.noreply.github.com>
2022-11-13 12:11:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+</head>
+<body>
+    <header class="header">
+        <a href="">
+            <img class="logo" src="images/5de9fc996c679202c505eb1adf8cedbf1.jpg">
+        </a>
+        <div class="burger-menu">
+            <span></span>
+        </div> 
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="list_item"><a href="">О нас</a></li>
+                <li class="list_item"><a href="">Главная</a></li>
+                <li class="list_item"><a href="">Документы</a></li>
+                <li class="list_item"><a href="">Личный кабинет</a></li>
+                <li class="list_item"><a href="">Заказы</a></li>
+            </ul>
+        </nav>    
+    </header>
+    <main>
+        <article class="text-block">
+            <h2 align="center" style="color:mediumturquoise">Герои</h2>
+            <img class="logo" src="images/Логотип.jpg">
+            <div class="carousel">
+                <div class="carousel_item">
+                    <img src="images/пик.jpg" alt="" class="image">
+                    <p class="пик"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/вару.jpg" alt="" class="image">
+                    <p class="вару"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/данте.jpg" alt="" class="image">
+                    <p class="данте"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/габри.jpg" alt="" class="image">
+                    <p class="габри"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/куро.jpg" alt="" class="image">
+                    <p class="куро"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/зонтик.jpg" alt="" class="image">
+                    <p class="зонтик"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/ромка.jpg" alt="" class="image">
+                    <p class="ромка"></p>
+                </div>
+                <div class="carousel_item">
+                    <img src="images/феликс.jpg" alt="" class="image">
+                    <p class="феликс"></p>
+                </div>
+            </div>
+            <p>Пример: земля королей - трефовый том.
+
+                <h2 align="left" style="color:lightskyblue">Данте: король бубен - филосов на расслабоне, глубина души которогоскрывает могущество.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Габриель: влет бубен - любопытный тихоня с нестандартным складом ума.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Пик: король пик - балансирует между желанием вести за собой и разочарованием в людях.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Вару: валет пик - постоянно иронизирующий провокатор, лжец и хитрец, истинная мотивация которого остаётся загадкой.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Куромаку: король треф( если кто не знает король крести) - технократ-прагматик, сторонник холодного расчёта и дипломатии. иногда его заносит от нарушения другими дисциплины.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Зонтик: валет треф - благожелательный нюня. но пускай его боязливый вид вас не смущает.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Ромео: король червей - открытый романтик-гедонист.
+            </h2>
+
+                <h2 align="left" style="color:lightskyblue">Феликс: валет червей - добро с кулаками, и этим всё сказано.
+            </h2>
+            </p>
+            <h2 align="center" style="color:mediumturquoise">Что в книге?</h2>
+            <p>Восемь карточных олицетворений, валеты и короли четырёх мастей с абсолютно разными характерами и темпераментами, оказались заперты друг с другом в замкнутом параллельном измерении, владея при этом мощным источником энергии, способным менять окружающую реальность. Территория и сила ограничены, в отличие от непомерного эго и амбиций некоторых героев. Как скоро возникнет конфликт и во что он выльется?</p>
+        </article>
+    </main>
+    <footer>
+        <article class="podval">
+            <h1 align="center" style="color:mediumturquoise">Подвал</h1>
+            <p>Создатель: Арлета Гаврилова</p>
+            <h3>Номер телефона: +371 29 650 875</h3>
+            <h4>место жительства:  страна: латвия, город: рига, улица: plostu iela 24.</h4>
+            <h1 align="center" style="color:rgb(0, 0, 0)">Подвал</h1>
+        </article>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Основные параметры WordPress.
+ *
+ * Скрипт для создания wp-config.php использует этот файл в процессе установки.
+ * Необязательно использовать веб-интерфейс, можно скопировать файл в "wp-config.php"
+ * и заполнить значения вручную.
+ *
+ * Этот файл содержит следующие параметры:
+ *
+ * * Настройки базы данных
+ * * Секретные ключи
+ * * Префикс таблиц базы данных
+ * * ABSPATH
+ *
+ * @link https://ru.wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Параметры базы данных: Эту информацию можно получить у вашего хостинг-провайдера ** //
+/** Имя базы данных для WordPress */
+define( 'DB_NAME', 'frame' );
+
+/** Имя пользователя базы данных */
+define( 'DB_USER', 'root' );
+
+/** Пароль к базе данных */
+define( 'DB_PASSWORD', '' );
+
+/** Имя сервера базы данных */
+define( 'DB_HOST', 'localhost' );
+
+/** Кодировка базы данных для создания таблиц. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Схема сопоставления. Не меняйте, если не уверены. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Уникальные ключи и соли для аутентификации.
+ *
+ * Смените значение каждой константы на уникальную фразу. Можно сгенерировать их с помощью
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ сервиса ключей на WordPress.org}.
+ *
+ * Можно изменить их, чтобы сделать существующие файлы cookies недействительными.
+ * Пользователям потребуется авторизоваться снова.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'guJTC3i!_Fy8Nj: tf}#SsXHb{xJ57P@3[n[mi`wK1u^jO!dkKz3B>#QjQa)2|5s' );
+define( 'SECURE_AUTH_KEY',  'uAyf9J{Km*q~UshqIf}Eh=*w&5I/l:u[vF:_0![iyck%_V%AFX>s[=Ku%Fnu.z)o' );
+define( 'LOGGED_IN_KEY',    'vK,jT-aSqzc8rv4hQee&FZBi1+AiynUd>5H@5Hn[M85KkmME(fU~qPy W7M{+R5g' );
+define( 'NONCE_KEY',        'Pv^dy0mCh>C.DJB35!WnpT}V(?!]<sc{,M<qTQnIx~Ou46&gl8TchMb]pSCj}9,M' );
+define( 'AUTH_SALT',        ',pTx/?Y]{j/e/TJ_eq#5IZ 3L:]jmzhQR&CFt!dJr$kVGcag}J,4u3DoFSHX[6P1' );
+define( 'SECURE_AUTH_SALT', 'ZoxjU]mZaqm3C1>.AJgTgGl0CS1p52F?JSMtYvn2CGeOw+R#wfY1lI[0EOI`G#su' );
+define( 'LOGGED_IN_SALT',   'P6a.A&z$Ke |O|r|n8#EDzD07`AorgQc(re/pr</55E%ge,mZCAV^Y7^3z3-9PcJ' );
+define( 'NONCE_SALT',       'i5>@z~Q#BU7zu=]n~Lz>%@l<o^f0`-Qa3F1j7?j}j/IW E/Lzgdv,*yjZqHTf|@<' );
+
+/**#@-*/
+
+/**
+ * Префикс таблиц в базе данных WordPress.
+ *
+ * Можно установить несколько сайтов в одну базу данных, если использовать
+ * разные префиксы. Пожалуйста, указывайте только цифры, буквы и знак подчеркивания.
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Для разработчиков: Режим отладки WordPress.
+ *
+ * Измените это значение на true, чтобы включить отображение уведомлений при разработке.
+ * Разработчикам плагинов и тем настоятельно рекомендуется использовать WP_DEBUG
+ * в своём рабочем окружении.
+ *
+ * Информацию о других отладочных константах можно найти в документации.
+ *
+ * @link https://ru.wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Произвольные значения добавляйте между этой строкой и надписью "дальше не редактируем". */
+
+
+
+/* Это всё, дальше не редактируем. Успехов! */
+
+/** Абсолютный путь к директории WordPress. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Инициализирует переменные WordPress и подключает файлы. */
+require_once ABSPATH . 'wp-settings.php';
